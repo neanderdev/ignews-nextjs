b0d24d8c3f1e070a3014989223ec8e3cd1cfee42
neanderdev <neander.faria@hotmail.com>
2022-02-24 15:12:47 -0300

    Creating the Application Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
-import Head from "next/head"
+import Head from "next/head";
+import styled from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+
+      <main className={styled.contentContainer}>
+        <section className={styled.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Set access to all the plublications <br />
+            <span>For $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
