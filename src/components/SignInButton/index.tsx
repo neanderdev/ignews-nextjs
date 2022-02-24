544b5541b2dc4da618f59ec8ae39cee569ae7c49
neanderdev <neander.faria@hotmail.com>
2022-02-24 16:58:31 -0300

    Creating authentication with Github in the application

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,8 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi"
-import styled from "./styles.module.scss"
+import { signIn } from "next-auth/react";
+
+import styled from "./styles.module.scss";
 
 export function SignInButton() {
     const isUserLoggedIn = true;
@@ -12,7 +14,11 @@ export function SignInButton() {
             <FiX color="#737380" className={styled.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styled.signInButton}>
+        <button
+            type="button"
+            className={styled.signInButton}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417" />
             Sign in with Github
         </button>
