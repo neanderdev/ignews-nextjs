5672f2c821610d1746b4eb42c93db044dc212e7a
neanderdev <neander.faria@hotmail.com>
2022-02-25 08:37:48 -0300

    Saving user in db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +19,25 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: {
+    secret: process.env.SIGNINGKEY,
+  },
+  callbacks: {
+    async signIn({ user }) {      
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: { email } }
+          )
+        );
+  
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
