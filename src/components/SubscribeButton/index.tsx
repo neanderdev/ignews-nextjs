d3294569f9c591006b2d8ce1d4fa824b7ee3c536
neanderdev <neander.faria@hotmail.com>
2022-02-25 09:12:15 -0300

    Creating application checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from "next-auth/react";
 import styled from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,10 +6,23 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession();
+
+    async function handleSubscribe() {
+        if (!session) {
+            signIn('github');
+            return;
+        }
+
+        // criação da checkout session
+        
+    }
+
     return (
         <button
             type="button"
             className={styled.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
