cacd2f2dc6f9788f06441a6900888ad4bb02adf4
neanderdev <neander.faria@hotmail.com>
2022-03-18 10:51:52 -0300

    Test more function SignInButton

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { mocked } from 'jest-mock';
-import { useSession } from 'next-auth/react';
+import { useSession, signOut, signIn } from 'next-auth/react';
 
 import { SignInButton } from '.';
 
@@ -37,5 +37,47 @@ describe('SignInButton component', () => {
         render(<SignInButton />)
 
         expect(screen.getByText('John Doe')).toBeInTheDocument()
+    })
+
+    it('log out the user', () => {
+        const useSessionMocked = mocked(useSession)
+        const signOutMocked = mocked(signOut)
+
+        useSessionMocked.mockReturnValueOnce({
+            data: {
+                user: {
+                    name: 'John Doe',
+                    email: 'john.doe@example.com'
+                },
+                expires: 'fake-expires'
+            },
+            status: 'authenticated'
+        })
+
+        render(<SignInButton />)
+
+        const signOutButton = screen.getByText('John Doe')
+
+        fireEvent.click(signOutButton)
+
+        expect(signOutMocked).toHaveBeenCalled()
+    })
+
+    it('login user', () => {
+        const useSessionMocked = mocked(useSession)
+        const signInMocked = mocked(signIn)
+
+        useSessionMocked.mockReturnValueOnce({
+            data: null,
+            status: 'unauthenticated'
+        })
+
+        render(<SignInButton />)
+
+        const signInButton = screen.getByText('Sign in with Github')
+
+        fireEvent.click(signInButton)
+
+        expect(signInMocked).toHaveBeenCalled()
     })
 })
